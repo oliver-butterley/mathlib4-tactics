314c3527e7687d865d848091563780782daeb5f3
Seasawher <tellagoodstory@icloud.com>
2024-04-14 02:59:50 +0900

    remove unused code

--- lakefile.lean
+++ lakefile.lean
@@ -10,17 +10,3 @@ package «all-tactics» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.7.0"
-
-def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
-  let out ← IO.Process.output {
-    cmd := cmd
-    args := args
-  }
-  let hasError := out.exitCode != 0
-  if hasError then
-    IO.eprint out.stderr
-  return hasError
-
-script generate do
-  if ← runCmd "lake" #["env", "lean", "--run", "Main.lean", ">", "tactics.md"] then return 1
-  return 0
