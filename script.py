d969f6c938c129df0dfd500d667ef18d4362b7ea
Seasawher <tellagoodstory@icloud.com>
2024-04-14 02:20:33 +0900

    add python script to format markdown file

---
+++ script.py
@@ -0,0 +1,19 @@
+import re
+
+def main():
+  file_path = "tactics.md"
+
+  pattern = r'syntax "(.*?)".*?\[(.*)\]'
+
+  replacement = r"# \1\nDefined in: `\2`"
+
+  with open(file_path, 'r', encoding='utf-8') as file:
+    content = "".join([re.sub(r'^\s\s', '', line) for line in file])
+
+  new_content = re.sub(pattern, replacement, content)
+
+  with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(new_content)
+
+if __name__ == '__main__':
+  main()
