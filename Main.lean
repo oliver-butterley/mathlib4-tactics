2468ab867d9a8bf3070f7cdd67bdd96c08461397
Seasawher <tellagoodstory@icloud.com>
2024-04-13 18:12:17 +0900

    generate all tactics

---
+++ Main.lean
@@ -0,0 +1,5 @@
+import Mathlib.Tactic
+#help tactic
+
+def main : IO Unit :=
+  IO.println ""
--- lakefile.lean
+++ lakefile.lean
@@ -9,8 +9,18 @@ package «all-tactics» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.7.0"
 
-@[default_target]
-lean_lib «AllTactics» where
-  -- add any library configuration options here
+def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
+  let out ← IO.Process.output {
+    cmd := cmd
+    args := args
+  }
+  let hasError := out.exitCode != 0
+  if hasError then
+    IO.eprint out.stderr
+  return hasError
+
+script generate do
+  if ← runCmd "lake" #["env", "lean", "--run", "Main.lean", ">", "tactics.md"] then return 1
+  return 0
