20c0009f4a29e0aec2236f15eb99edab34946561
Seasawher <tellagoodstory@icloud.com>
2024-05-28 20:28:58 +0900

    fix branch of mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -9,4 +9,4 @@ package «mathlib4-tactics» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
